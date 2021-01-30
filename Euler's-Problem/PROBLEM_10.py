c6edd1b384392ca451c0c39b721890e240bc2c7b
prem <shout2lord7@gmail.com>
2021-01-30 11:42:44 +0545

    euler's 10

--- Euler's-Problem/PROBLEM_10.py
+++ Euler's-Problem/PROBLEM_10.py
@@ -20,4 +20,21 @@ for i in range(0,len(l)):
     total=total+l[i]
 
 print("sum = ",total)
+
+#another way 
+
+
+total=0
+
+for num in range(1,2000001):  
+    count = 0
+    for i in range(2, (num//2 + 1)):
+        if(num % i == 0):
+            count = count + 1
+            break
+
+    if (count == 0 and num != 1):
+        total = total + num
+
+print("sum = ",total)
 #I was using jupyter notebook, sum was printed intil 200000 and then after for 2 mil it stoped.
